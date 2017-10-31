a7abd3227d4c5303330a59d81dcea27135d8b173
SUJITHA7YERUVA <sujithayeruva198@gmail.com>
2017-10-31 15:43:17 +0100

    change1

--- src/project2/Main.java
+++ src/project2/Main.java
@@ -1,9 +1,14 @@
 package project2;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the task");
+		sc.nextLine();
+		
 
 	}
 
