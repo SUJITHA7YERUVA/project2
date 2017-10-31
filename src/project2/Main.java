99165aa529e1ae32f9c5150cf1528a8d61271e03
SUJITHA7YERUVA <sujithayeruva198@gmail.com>
2017-10-31 17:07:35 +0100

    changed2

--- src/project2/Main.java
+++ src/project2/Main.java
@@ -1,15 +1,50 @@
 package project2;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("enter the task");
-		sc.nextLine();
+		System.out.println("enter gym date" );
+		 String date = sc.nextLine();
+		 System.out.println( date );
+		List<LocalDate> datelist =new ArrayList();
+		
+		LocalDate today = LocalDate.now();
+	
+		;
+		LocalDate tommarow = today.plusDays(1);
+	LocalDate ysterday = today.minusDays(1);
+	   
+		
+	
+		if(today.isAfter(tommarow) ) {
+			System.out.println("nice logic");
+			
+		}
+		else {
+			System.out.println("its not correct");
+		}
+
+	
+		datelist.add(today);
+		 datelist.add(tommarow);
+	
+		for (int i = 0; i < datelist.size(); i++) {
+			LocalDate tempdart = datelist.get(i);
+			System.out.println(datelist);
+			datelist.remove(today);
+			System.out.println("***********************");
+			//datelist.sort(null);
+			
+		}	
 		
 
 	}
+	}
 
-}
